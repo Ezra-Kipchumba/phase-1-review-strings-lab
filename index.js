36d6fbcfff941f744a13ddb8b519292d7b21ab5d
ezra.kipchumba@student.moringaschool.com <ezra.kipchumba@student.moringaschool.com>
2022-08-20 11:15:05 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+let currentUser = "Ezra Kipchumba";
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!" ;
+currentUser = "EZRA KIPCHUMBA";
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, "+ currentUser + "!" ;
+currentUser = "Ezra Kipchumba!";
+const shortGreeting = "Welcome, " + currentUser;
