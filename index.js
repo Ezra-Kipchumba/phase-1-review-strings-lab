7c7deefb8ecf88809ff112202ad1623280f668a7
ezra.kipchumba@student.moringaschool.com <ezra.kipchumba@student.moringaschool.com>
2022-08-24 17:51:38 +0300

    complete

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 // Write your code in this file!
-let currentUser = "Ezra Kipchumba";
-let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!" ;
-currentUser = "EZRA KIPCHUMBA";
-const excitedWelcomeMessage = "WELCOME TO FLATBOOK, "+ currentUser + "!" ;
-currentUser = "Ezra Kipchumba!";
-const shortGreeting = "Welcome, " + currentUser;
+const currentUser = "Ezra Kipchumba";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!";
+const shortGreeting = "Welcome, " + "E" + "!";
